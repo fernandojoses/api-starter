e82b3ba93f8011b73017e12eedb413e96cd9e32b
Fernando Sagastume Sagastume <fjsaga60>
2020-03-06 11:18:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 
 URL = "http://api.open-notify.org/astros.json"
-
+URL ="https://swapi.co/api/PEOPLE/1/"
 res = requests.get(URL) # get the data
 res = res.json() # convert data to Python format
 
